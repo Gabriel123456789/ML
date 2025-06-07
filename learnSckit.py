dfe58118397211e17842058d9d923c93841adacc
Gabriel123456789 <gabrielborges0210@gmail.com>
2025-06-07 11:58:27 -0300

    Scalling data and tuning the model

--- learnSckit.py
+++ learnSckit.py
@@ -22,7 +22,9 @@ def preprocess_data(dataframe):
     dataframe["Embarked"].fillna("S", inplace = True) #inplace changes in the original data
     dataframe.drop(columns=["Embarked"], inplace = True)
     
-
+    ##add the age filling function
+    fill_ages(dataframe)
+    
     #convert gender to numbers
     dataframe["Sex"] = dataframe["Sex"].map({"male":1,"female":0})
     
@@ -37,7 +39,7 @@ def preprocess_data(dataframe):
     
     return dataframe
 
-# need to fill the ages
+# need to fill the ages that are missing
 
 def fill_ages(dataframe):
     #will treat this with the mean of age in the class they were in
@@ -48,3 +50,38 @@ def fill_ages(dataframe):
     dataframe["Age"] = dataframe.apply(lambda row:age_fill_map[row["Pclass"]] if pd.isnull(row["Age"]) else row["Age"], axis=1) #
     #above: the lambda receives each row(axis = 1) and attributes the value in the dictionary(median) if
     # the place it is is null, otherwise it keeps the row age value
+
+
+# applying the preprocessing to the data
+data  = preprocess_data(data)
+
+# separate the data from the desired prediction data
+X = data.drop(columns="Survived")
+Y = data["Survived"]
+
+
+# Separate data for training and testing
+x_training, x_testing, y_training, y_testing = train_test_split(X,Y, test_size=0.25, random_state=42)
+
+# Scale the data - Check if the data is ready for the model
+scaler = MinMaxScaler()
+x_training = scaler.fit_transform(x_training)
+x_testing = scaler.transform(x_testing)
+
+
+# Parameter Tuning - kNN - Defines the parameters used by our model
+def tune_model(x_training, y_training):
+    param_grid = {
+        "n_neighbors": range(1,21), #the n number/parameter for knn changes
+        "metrics": ["euclidean", "manhattan", "minkowski"], #different forms of calculating distance from neighbors
+        "weights": ["uniform", "distance"]
+    }
+    
+    #build the model itself
+    model = KNeighborsClassifier()
+    
+    # Perform a grid search - uses the tune model to see wich parameters perfom better
+    grid_search = GridSearchCV(model,param_grid, cv=5, n_jobs=-1)
+    grid_search.fit(x_training,y_training) # Here we are training the grid search with our data
+    
+    return grid_search.best_estimator_
